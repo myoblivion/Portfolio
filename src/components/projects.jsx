bee9baffe257b6224b56479ceaf7ac2bbcd5c18b
chuuuya12 <96934806+myoblivion@users.noreply.github.com>
2022-01-15 17:48:45 +0800

    commit

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,22 +10,46 @@ const ProjectsComponent = () => {
         <h1>Projects</h1>
         <div className="grid">
           <div className="card">
-            <div className="card__details"><a href="https://myoblivion.github.io/frontend-time-tracker/">Frontend Mentor | Time Tracker</a></div>
+            <div className="card__details">
+              <a href="https://myoblivion.github.io/frontend-time-tracker/">
+                Frontend Mentor | Time Tracker
+              </a>
+            </div>
           </div>
           <div className="card">
-            <div className="card__details"><a href="https://myoblivion.github.io/order/">Frontend Mentor | Order Summary</a></div>
+            <div className="card__details">
+              <a href="https://myoblivion.github.io/order/">
+                Frontend Mentor | Order Summary
+              </a>
+            </div>
           </div>
           <div className="card">
-            <div className="card__details"><a href="https://myoblivion.github.io/three-column-preview-card/">Frontend Mentor | Three Column Preview Card</a></div>
+            <div className="card__details">
+              <a href="https://myoblivion.github.io/three-column-preview-card/">
+                Frontend Mentor | Three Column Preview Card
+              </a>
+            </div>
           </div>
           <div className="card">
-            <div className="card__details"><a href="https://myoblivion.github.io/fem-tip-calculator/">Frontend Mentor | Tip Calculator</a></div>
+            <div className="card__details">
+              <a href="https://myoblivion.github.io/fem-tip-calculator/">
+                Frontend Mentor | Tip Calculator
+              </a>
+            </div>
           </div>
           <div className="card">
-            <div className="card__details"><a href="https://myoblivion.github.io/stats-preview-card/">Frontend Mentor | Stats Preview Card</a></div>
+            <div className="card__details">
+              <a href="https://myoblivion.github.io/stats-preview-card/">
+                Frontend Mentor | Stats Preview Card
+              </a>
+            </div>
           </div>
           <div className="card">
-            <div className="card__details"><a href="https://myoblivion.github.io/space-tour-fem/#/">Frontend Mentor | Space Tourism</a></div>
+            <div className="card__details">
+              <a href="https://myoblivion.github.io/space-tour-fem/#/">
+                Frontend Mentor | Space Tourism
+              </a>
+            </div>
           </div>
         </div>
       </div>
